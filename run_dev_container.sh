0ebd6b7b6c439c142189ad1683e93f32194d0c6e
Jon Bacon <bacon.jon@gmail.com>
2026-01-12 18:40:48 +0000

    Fix run script

--- run_dev_container.sh
+++ run_dev_container.sh
@@ -9,7 +9,7 @@ GID=$(id -g)
 
 CONTAINER_NAME=jb_dev_container_0
 
-if [ $(docker container inspect -f '{{.State.Running}}' $CONTAINER_NAME) = "false" ]; then
+if [ $(docker container inspect -f '{{.State.Running}}' $CONTAINER_NAME 2>&1) != "true" ]; then
   docker run --rm -dit -v ${HOME}:${HOME} --name $CONTAINER_NAME jb_dev_container:0.1
 
   docker exec $CONTAINER_NAME bash -c "groupadd -g $GID $GROUPNAME"
